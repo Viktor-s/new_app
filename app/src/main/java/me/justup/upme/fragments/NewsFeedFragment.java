38fcad1450c92e1e80c23e51e9b5eff4c9c43e38
initrod <initrod@gmail.com>
2015-01-21 17:18:34 +0200

    Work with news fragment

--- app/src/main/java/me/justup/upme/fragments/NewsFeedFragment.java
+++ app/src/main/java/me/justup/upme/fragments/NewsFeedFragment.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 
 import java.util.List;
@@ -63,9 +65,13 @@ public class NewsFeedFragment extends Fragment {
             @Override
             public void onItemClick(View view, int position) {
                 if (lastChosenPosition != position) {
+
+
+                    Animation mFragmentSliderFadeIn = AnimationUtils.loadAnimation(AppContext.getAppContext(), R.anim.fragment_item_slide_fade_in);
                     final FragmentTransaction ft = getChildFragmentManager().beginTransaction();
                     ft.replace(R.id.news_item_container_frameLayout, NewsItemFragment.newInstance(mNewsFeedEntityList.get(position)));
                     ft.commit();
+                    mNewsItemContainer.startAnimation(mFragmentSliderFadeIn);
                     lastChosenPosition = position;
                 }
             }
