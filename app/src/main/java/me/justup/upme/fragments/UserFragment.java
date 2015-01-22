3b0184a9f73321d3890b658b39f83699fa20a448
initrod <initrod@gmail.com>
2015-01-22 21:41:49 +0200

    Google maps, user panel, add to gradle release version

--- app/src/main/java/me/justup/upme/fragments/UserFragment.java
+++ app/src/main/java/me/justup/upme/fragments/UserFragment.java
@@ -6,12 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import me.justup.upme.R;
 
 import static me.justup.upme.utils.LogUtils.makeLogTag;
 
 
-public class UserFragment extends Fragment {
+public class UserFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = makeLogTag(UserFragment.class);
 
 
@@ -19,8 +26,23 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
 
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng city = new LatLng(49.99356, 36.239519);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(city, 15));
+
+        map.addMarker(new MarkerOptions()
+                .title("Kharkov")
+                .snippet("AppDragon")
+                .position(city));
     }
 
 }
