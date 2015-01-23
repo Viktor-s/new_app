a54dcddf42c249ef88445370ab74648afd56cfe5
bogdan <bodja.bat@gmail.com>
2015-01-23 20:02:09 +0200

    UI briefcase

--- app/src/main/java/me/justup/upme/ProductItemActivity.java
+++ app/src/main/java/me/justup/upme/ProductItemActivity.java
@@ -2,6 +2,10 @@ package me.justup.upme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 /**
  * Created by bogdan on 19.01.15.
@@ -11,6 +15,27 @@ public class ProductItemActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_products_without_bail);
+        setContentView(R.layout.fragment_products_item);
+
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.container_product_item);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        LinearLayout productsWithoutBail = (LinearLayout) inflater.inflate(R.layout.fragment_products_without_bail, null, false);
+        LinearLayout productsBail = (LinearLayout) inflater.inflate(R.layout.fragment_products_bail, null, false);
+        LinearLayout productsLifeInsurance = (LinearLayout) inflater.inflate(R.layout.fragment_life_insurance, null, false);
+        LinearLayout productsRiskInsurance = (LinearLayout) inflater.inflate(R.layout.fragment_risk_insurance, null, false);
+        LinearLayout productsAirlineTickets = (LinearLayout) inflater.inflate(R.layout.fragment_airline_tickets, null, false);
+        LinearLayout productsEducationOnline = (LinearLayout) inflater.inflate(R.layout.fragment_education_online, null, false);
+
+        rl.addView(productsWithoutBail);
+
+        CheckBox checkBox = (CheckBox) findViewById(R.id.check_lock);
+
+
+
+
+
+
     }
 }
