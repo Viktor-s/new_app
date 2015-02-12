b15e36b589763ea79bc66228f9e024416d84d22d
initrod <initrod@gmail.com>
2015-02-12 18:56:09 +0200

    Settings main screen rebuild, file explorer

--- app/src/main/java/me/justup/upme/fragments/DocumentsFragment.java
+++ app/src/main/java/me/justup/upme/fragments/DocumentsFragment.java
@@ -1,11 +1,14 @@
 package me.justup.upme.fragments;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.File;
@@ -18,25 +21,34 @@ import static me.justup.upme.utils.LogUtils.makeLogTag;
 public class DocumentsFragment extends Fragment {
     private static final String TAG = makeLogTag(DocumentsFragment.class);
 
+    private LinearLayout mFileExplorer;
+    private LayoutInflater mLayoutInflater;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_documents, container, false);
 
-        TextView mFileExplorer = (TextView) view.findViewById(R.id.files_dirs_textView);
+        mLayoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mFileExplorer = (LinearLayout) view.findViewById(R.id.files_panel);
 
         File mStorageDirectory = Environment.getExternalStorageDirectory();
         File[] mDirList = mStorageDirectory.listFiles();
 
-        StringBuilder mList = new StringBuilder();
-
         for (File file : mDirList) {
-            mList.append(file.getPath()).append("\t\t[").append(file.getName()).append("]\n");
+            if (!file.isDirectory()) {
+                setFileItem(file.getName());
+            }
         }
 
-        mFileExplorer.setText(mList.toString());
-
         return view;
+    }
+
+    @SuppressLint("InflateParams")
+    private void setFileItem(String fileName) {
+        final View item = mLayoutInflater.inflate(R.layout.item_documents_file, null);
+        ((TextView) item.findViewById(R.id.file_name_textView)).setText(fileName);
+        mFileExplorer.addView(item);
     }
 
 }
