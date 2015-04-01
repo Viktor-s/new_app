ff795198cf8c29d027264578cc6fbbf4f5df9fec
initrod <initrod@gmail.com>
2015-04-01 16:16:34 +0300

    Remove user panel and FastPaying

--- app/src/main/java/me/justup/upme/MainActivity.java
+++ app/src/main/java/me/justup/upme/MainActivity.java
@@ -50,7 +50,6 @@ import me.justup.upme.fragments.MailFragment;
 import me.justup.upme.fragments.NewsFeedFragment;
 import me.justup.upme.fragments.ProductsFragment;
 import me.justup.upme.fragments.StudyFragment;
-import me.justup.upme.fragments.UserFragment;
 import me.justup.upme.fragments.WebRtcFragment;
 import me.justup.upme.http.ApiWrapper;
 import me.justup.upme.http.HttpIntentService;
@@ -155,9 +154,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
         makeButtonSelector();
 
         ApiWrapper.query(new GetLoggedUserInfoQuery(), new OnGetLoggedUserInfoResponse());
-
-        Fragment fragment = UserFragment.newInstance(new GetLoggedUserInfoQuery(), true);
-        getFragmentManager().beginTransaction().add(R.id.mapAndUserFragment, fragment).commit();
 
         View mOpenStatusBar = findViewById(R.id.status_bar_fragment);
         mOpenStatusBar.setOnClickListener(new OnOpenStatusBarListener());
